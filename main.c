d535a42dba6141074e262825232c5828b54f8f4f
John <john@john2143.com>
2016-12-21 16:58:45 -0500

    now its not actually a raytracer (dont tell anyone)

--- main.c
+++ main.c
@@ -7,108 +7,237 @@
 #include <time.h>
 #include <matrix.c>
 
-void setPixel(uint32_t *image, int h, int x, int y, int r, int g, int b){
-    uint32_t pixel = 0xFF000000;
-    pixel |= r << 16;
-    pixel |= g << 8;
-    pixel |= b << 0;
-    image[y * h + x] = pixel;
+int w = 1000, h = 1000;
+uint32_t color = 0xFF000000;
+
+uint32_t col(int r, int g, int b){
+    uint32_t c = 0xFF000000;
+    c |= r << 16;
+    c |= g << 8;
+    c |= b << 0;
+    return c;
+}
+
+void setColor(int r, int g, int b){
+    color = col(r, g, b);
+}
+
+void setPixel(uint32_t *image, int h, int x, int y){
+    image[y * h + x] = color;
 }
 
 #define PI 3.1415926
 
 float map(float val, float stval, float enval, float newst, float newen){
     float range = enval - stval;
+    if(range == 0) return newst;
     float pct = (val - stval) / range;
     float newrange = newen - newst;
     return newrange * pct + newst;
 }
 
-matrix4 transformMatrix;
-
-vec3 spheres[1] = {
-    {0, 0, 10}
-};
-
-#define numspheres (sizeof(spheres) / sizeof(*spheres))
-vec3 realSpheres[numspheres];
-
-const float rayDist = 30;
-const float radius = 2;
-
-float raytraceStep(float yaw, float pitch){
-    float dist = 0;
-    const float delta = .1;
-    vec3 pos = {0, 0, 0};
-    vec3 deltaVec = aimToVec(yaw, pitch);
-
-    for(; dist < rayDist; dist += delta){
-        for(size_t i = 0; i < numspheres; i++){
-            if(length3(addVec3(scaleVec3(pos, -1), realSpheres[i])) < radius*radius){
-                return sqrt(dist);
-            }
-        }
-        pos = addVec3(pos, deltaVec);
-    }
-    return rayDist;
-}
-
-float raytraceToSpheres(float yaw, float pitch){
-    float min = rayDist;
-    for(size_t i = 0; i < numspheres; i++){
-        vec3 circle = realSpheres[i];
-        if(circle.z < 0) continue;
-        float distxz = circle.x - tan(-yaw) * circle.z;
-        float distyz = circle.y - tan(pitch) * circle.z;
-        /*float disty = 0;*/
-        float distsq = distxz * distxz + distyz * distyz;
-        float r2 = radius * radius;
-        if(distsq < r2){
-            float delz1 = sqrt(r2 - distxz * distxz);
-            float delz2 = sqrt(r2 - distyz * distyz);
-            float newmin = length3(circle);
-            if(newmin < min) min = newmin;
-        }
-    }
-    return min;
-}
-
-/*#define sgn(x) ((x) < 0 ? -1 : 1)*/
-
-/*float raytraceToSpheresDepthCirc(float yaw, float pitch){*/
-    /*float mindist = rayDist;*/
-    /*for(size_t i = 0; i < numspheres; i++){*/
-        /*yaw = yaw + PI/2;*/
-        /*vec3 circ = realSpheres[i];*/
-        /*float D = circ.z * (cos(yaw) - circ.x) - circ.x * (sin(yaw) - circ.z);*/
-        /*float P = radius * radius - D * D;*/
-        /*if(P < 0) continue;*/
-        /*vec2 point;*/
-        /*float SP = sqrt(P);*/
-        /*point.x = D * sin(yaw) - cos(yaw) * SP;*/
-        /*point.y = D * cos(yaw) - sin(yaw) * SP;*/
-        /*mindist = length2(point);*/
-    /*}*/
-    /*return mindist;*/
-/*}*/
-
-/*float raytraceToSpheres(float yaw, float pitch){*/
-    /*return 4;*/
-/*}*/
-
-void drawLine(uint32_t *image, int h, int x, int y, int x2, int y2, int r, int g, int b){
+void drawLine(uint32_t *image, int h, int x, int y, int x2, int y2){
     float loops = 20;
     for(int i = 0; i < loops; i++){
         float xx = map(i, 0, loops, x, x2);
         float yy = map(i, 0, loops, y, y2);
-        setPixel(image, h, xx, yy, r, g, b);
+        setPixel(image, h, xx, yy);
     }
+}
+
+#define min(x, y) ((x) < (y) ? (x) : (y))
+#define max(x, y) ((x) > (y) ? (x) : (y))
+
+void blitVertLine(uint32_t *image, int h, int x, int y1, int y2){
+    if(x < 0 || x > w) return;
+    int start = max(min(y1, y2), 0);
+    int end   = min(max(y1, y2), h);
+    for(int i = start; i < end; i++){
+        setPixel(image, h, x, i);
+    }
+}
+
+void blitTriangle(uint32_t *image, int h, matrix3 pos){
+    if(pos.x.z < 0 || pos.y.z < 0 || pos.z.z < 0) return;
+    if(pos.y.x < pos.x.x){
+        vec3 temp = pos.x;
+        pos.x = pos.y;
+        pos.y = temp;
+    }
+
+    if(pos.z.x < pos.y.x){
+        vec3 temp = pos.y;
+        pos.y = pos.z;
+        pos.z = temp;
+    }
+
+    if(pos.y.x < pos.x.x){
+        vec3 temp = pos.x;
+        pos.x = pos.y;
+        pos.y = temp;
+    }
+
+    /*printMatrix3(pos);*/
+    /*printf("\n\n");*/
+
+    int x;
+    for(x = pos.x.x; x <= pos.y.x; x++){
+        blitVertLine(image, h, x,
+                map(x, pos.x.x, pos.y.x, pos.x.y, pos.y.y),
+                map(x, pos.x.x, pos.z.x, pos.x.y, pos.z.y)
+        );
+    }
+    for(; x < pos.z.x; x++){
+        blitVertLine(image, h, x,
+                map(x, pos.y.x, pos.z.x, pos.y.y, pos.z.y),
+                map(x, pos.x.x, pos.z.x, pos.x.y, pos.z.y)
+        );
+    }
+}
+
+vec3 shape[1000];
+int shapesize;
+
+#define beginShape() shapesize = 0;
+#define vertex(x, y, z) shape[shapesize++] = (const vec3) {.d = {x, y, z}};
+#define endShape() \
+{ \
+    int i = 0; \
+    for(; i < shapesize - 2; i++) { \
+        matrix3 matr = {.row = { shape[i], shape[i + 1], shape[i + 2] }}; \
+        blitTriangle(pixels, h, matr); \
+    } \
+}
+
+matrix4 transformMatrix, id4;
+vec3 transpose;
+
+void translateShapes(){
+    for(int i = 0; i < shapesize; i++) {
+        vec4 hom = homogonize3(addVec3(shape[i], transpose));
+        hom = multMatVec4(transformMatrix, hom);
+        shape[i] = dehomogonize4(hom);
+        if(shape[i].z < 0) return;
+        shape[i] = scaleVec3(shape[i], 1/shape[i].z);
+        shape[i].x = map(shape[i].x, -1, 1, 0, w);
+        shape[i].y = map(shape[i].y, -1, 1, h, 0);
+    }
+}
+
+#define printShape() \
+    for(int i = 0; i < shapesize; i++){ \
+        printVec3(shape[i]); \
+        printf("\n"); \
+    }
+
+#define numx 400
+#define numy 100
+#define maxvar 1
+float heightMap[numx][numy] = {};
+uint32_t colors[numy] = {};
+
+#define maxx 40
+#define minx -maxx
+
+void draw(uint32_t *pixels, int h, float offset, float yoffset, vec3 charPos, int frames){
+    transformMatrix = id4;
+    transpose = scaleVec3(charPos, -1);
+    matrix4 rotateMatrix = {.d = {
+        {1, 0, 0, 0},
+        {0, cos(yoffset), -sin(yoffset), 0},
+        {0, sin(yoffset), cos(yoffset), 0},
+        {0, 0, 0, 1}
+    }};
+    transformMatrix = multMatMat4(transformMatrix, rotateMatrix);
+    matrix4 rotateMatrix2 = {.d = {
+        {cos(-offset), 0, sin(-offset), 0},
+        {0, 1, 0, 0},
+        {-sin(-offset), 0, cos(-offset), 0},
+        {0, 0, 0, 1}
+    }};
+    transformMatrix = multMatMat4(transformMatrix, rotateMatrix2);
+
+    for(int i = 0; i < w * h; i++){
+        pixels[i] = 0xFF000000;
+    }
+    for(int z = numy; z >= 1; z--){
+        beginShape();
+        color = colors[z];
+        for(int x = 0; x < numx; x++){
+            float xx = map(x, 0, numx, minx, maxx);
+            vertex(xx, heightMap[x][z], z + 3);
+            vertex(xx, heightMap[x][z - 1], z - 1 + 3);
+        }
+        translateShapes();
+        endShape();
+    }
+}
+
+uint32_t hsv2rgb(float hue, float sat, float val)
+{
+    double      p, q, t, ff;
+    long        i;
+    struct rgb{
+        float r, g, b;
+    } out;
+
+    hue /= 60.0;
+    i = (long) hue;
+    p = val * (1.0 - sat);
+    q = val * (1.0 - (sat * hue));
+    t = val * (1.0 - (sat * (1.0 - hue)));
+
+    switch(i) {
+    case 0:
+        out.r = val;
+        out.g = t;
+        out.b = p;
+        break;
+    case 1:
+        out.r = q;
+        out.g = val;
+        out.b = p;
+        break;
+    case 2:
+        out.r = p;
+        out.g = val;
+        out.b = t;
+        break;
+
+    case 3:
+        out.r = p;
+        out.g = q;
+        out.b = val;
+        break;
+    case 4:
+        out.r = t;
+        out.g = p;
+        out.b = val;
+        break;
+    case 5:
+    default:
+        out.r = val;
+        out.g = p;
+        out.b = q;
+        break;
+    }
+    return col(out.r * 255, out.g * 255, out.b * 255);
 }
 
 int main(int argc, char **argv){
     (void) argc; (void) argv;
     srand(time(NULL));
-    int w = 1000, h = 1000;
+
+    id4 = identity4();
+
+    for(int j = 0; j < numy; j++){
+        for(int i = 0; i < numx; i++){
+            heightMap[i][j] = map(randomFloat(), 0, 1, -maxvar, maxvar);
+            /*heightMap[i][j] = map(j, 0, numy, -maxvar, maxvar);*/
+        }
+        int sat = map(j, 0, numy, 100, 255);
+        colors[j] = hsv2rgb(map(j, 0, numy, 0, 360), .5, 1);
+    }
 
     const vec3 test = {.d = {1, 2, 3}};
     printf("Testt\n");
@@ -211,7 +340,13 @@ int main(int argc, char **argv){
                 } break;
                 case SDL_SCANCODE_A:{
                     charPos.x -= cos(offset) * .1;
-                    charPos.z -= sin(offset) * .1;
+                    charPos.z += sin(offset) * .1;
+                } break;
+                case SDL_SCANCODE_LSHIFT:{
+                    charPos.y -= .1;
+                } break;
+                case SDL_SCANCODE_SPACE:{
+                    charPos.y += .1;
                 } break;
                 case SDL_SCANCODE_Q:
                     goto CLEANUP;
@@ -228,82 +363,19 @@ int main(int argc, char **argv){
 
 
             /*vec3 diff = aimToVec(offset, yoffset);*/
-            /*printf("View: ");*/
-            /*[>printVec3(diff);<]*/
-            /*printf("yaw %f, pit %f", offset, yoffset);*/
+            printf("View: ");
+            /*[>[>printVec3(diff);<]<]*/
+            printf("yaw %f, pit %f", offset, yoffset);
             /*printf(" Pos: ", offset, yoffset);*/
             /*printVec3(charPos);*/
-            /*printf("\r");*/
+            printf("\r");
         }
 
         uint32_t *pixels;
         int pitch;
         SDL_LockTexture(texture, NULL, (void **) &pixels, &pitch);
 
-
-        vec4 homospheres[numspheres];
-        for(size_t i = 0; i < numspheres; i++){
-            homospheres[i] = homogonize3(spheres[i]);
-        }
-
-#define TRANSFORM 1
-#if TRANSFORM
-        transformMatrix = homoTrans(charPos);
-
-        matrix4 rotateMatrix = {.d = {
-            {1, 0, 0, 0},
-            {0, cos(yoffset), -sin(yoffset), 0},
-            {0, sin(yoffset), cos(yoffset), 0},
-            {0, 0, 0, 1}
-        }};
-        transformMatrix = multMatMat4(transformMatrix, rotateMatrix);
-        matrix4 rotateMatrix2 = {.d = {
-            {cos(offset), 0, sin(offset), 0},
-            {0, 1, 0, 0},
-            {-sin(offset), 0, cos(offset), 0},
-            {0, 0, 0, 1}
-        }};
-        transformMatrix = multMatMat4(transformMatrix, rotateMatrix2);
-
-        for(size_t i = 0; i < numspheres; i++){
-            homospheres[i] = multMatVec4(transformMatrix, homospheres[i]);
-        }
-
-#endif
-        for(size_t i = 0; i < numspheres; i++){
-            realSpheres[i] = dehomogonize4(homospheres[i]);
-        }
-
-#define precision 10
-
-        for(int y = 0; y < h; y += precision){
-            for(int x = 0; x < w; x += precision){
-                float yaw   = map(x, 0, w, -1, 1);
-                float pitch = map(y, 0, h, 1, -1);
-
-                yaw = atan(yaw);
-                pitch = atan(pitch);
-
-                float dist = raytraceToSpheres(yaw, pitch);
-                /*float dist = raytraceStep(yaw, pitch);*/
-                unsigned char r = map(dist, 0, 30, 255, 0);
-                unsigned char g = map(dist, 0, 30, 255, 0);
-                unsigned char b = map(dist, 0, 30, 255, 0);
-                for(int i = 0; i < precision; i++){
-                    for(int j = 0; j < precision; j++){
-                        setPixel(pixels, h, x + i, y + j, r, g, b);
-                    }
-                }
-            }
-        }
-
-        /*//Draw unit vectors*/
-
-        /*vec3 aim = aimToVec(offset, yoffset);*/
-
-        /*drawLine(pixels, h, 30, 30,*/
-                /*aim.x/aim.z * 20 + 30, aim.y/aim.z * 20 + 30,*/
-                /*000, 000, 255);*/
+        draw(pixels, h, offset, yoffset, charPos, i);
 
         SDL_UnlockTexture(texture);
 
